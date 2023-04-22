318ca358d421427bb4c21042e7a5e8b7dcfddd71
Manav Chawla <manav.chawla@sifars.com>
2023-04-23 00:28:37 +0530

    changes done

--- src/Todos.js
+++ src/Todos.js
@@ -1,24 +1,33 @@
-import React from 'react';
+import React from "react";
 
-const Todos = ({todos, markComplete, editTitle}) => {
-    return (
-        <div className={"todo-list"}>
-                {
-                    todos.map((todo, index) => (
-                        <Todo todo={todo} key={index} index={index} markComplete={markComplete} editTitle={editTitle}/>
-                    ))
-                }
-        </div>
-    );
+const Todos = ({ todos, markComplete, editTitle }) => {
+  return (
+    <div className={"todo-list"}>
+      {todos.map((todo, index) => (
+        <Todo
+          todo={todo}
+          key={index}
+          index={index}
+          markComplete={markComplete}
+          editTitle={editTitle}
+        />
+      ))}
+    </div>
+  );
 };
 
-const Todo = ({todo, index, markComplete, editTitle}) =>(
-    <div className="todo">
-        <p style={{textDecoration : todo.isCompleted ? "line-through" : ""}}>
-            <input type={"checkbox"} onChange={() => markComplete(index)} name={"completed"} id={todo.id}/>{' '}
-            {todo.text}
-        </p>
-    </div>
+const Todo = ({ todo, index, markComplete, editTitle }) => (
+  <div className="todo">
+    <p style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+      <input
+        type={"checkbox"}
+        onChange={() => markComplete(index)}
+        name={"completed"}
+        id={todo.id}
+      />{" "}
+      {todo.text}
+    </p>
+  </div>
 );
 
 export default Todos;
